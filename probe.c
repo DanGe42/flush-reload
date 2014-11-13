d3748da177274ea59f91f91f8c659fe3e5a08605
David Mally <djmally@gmail.com>
2014-11-13 00:05:04 -0500

    Added provided code from paper (threshold isn't a thing yet)

---
+++ probe.c
@@ -0,0 +1,20 @@
+int probe(char *adrs) {
+    volatile unsigned long time;
+
+    asm __volatile__(
+        "    mfence             \n"
+        "    lfence             \n"
+        "    rdtsc              \n"
+        "    lfence             \n"
+        "    movl %%eax, %%esi  \n"
+        "    movl (%1), %%eax   \n"
+        "    lfence             \n"
+        "    rdtsc              \n"
+        "    subl %%esi, %%eax  \n"
+        "    clflush 0(%1)      \n"
+        : "=a" (time)
+        : "c" (adrs)
+        : "%esi", "%edx"
+    );
+    return time < threshold;
+}
