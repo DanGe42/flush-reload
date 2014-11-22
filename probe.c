3992077795af8356c1db49417383f91bd45818bb
Daniel Ge <dange@seas.upenn.edu>
2014-11-22 13:31:30 -0500

    Implement mmap skeleton

--- probe.c
+++ probe.c
@@ -1,3 +1,13 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+// Roughly 4 MB. Always ensure that the executable is smaller than this
+#define GPG_MAX_SIZE_BYTES 4194304
+
+#define PROBE_THRESHOLD 120ul
+
 int probe(char *adrs) {
     volatile unsigned long time;
 
@@ -16,5 +26,40 @@ int probe(char *adrs) {
         : "c" (adrs)
         : "%esi", "%edx"
     );
-    return time < threshold;
+    return time < PROBE_THRESHOLD;
 }
+
+void spy(void *gpg_base) {
+    // TODO
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s GPGpath\n", argv[0]);
+        return 1;
+    }
+
+    char *gpg_path = argv[1];
+    int gpg_fd = open(gpg_path, O_RDONLY);
+    if (gpg_fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    // memory map so we can force OS to share this memory page with GPG process
+    size_t map_len = GPG_MAX_SIZE_BYTES;
+    void *gpg_base = mmap(NULL, map_len, PROT_READ, MAP_FILE | MAP_SHARED,
+            gpg_fd, 0);
+    if (gpg_base == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+    spy(gpg_base);
+
+    // Probably never reached because we'll likely just ^C the program. Maybe
+    // implement a SIGTERM / SIGINT handler?
+    munmap(gpg_base, map_len);
+    close(gpg_fd);
+    return 0;
+}
+
