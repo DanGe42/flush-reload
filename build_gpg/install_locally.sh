83cfe8799c2fb1592365a66fe2c980d03f20133e
Daniel Ge <dange@seas.upenn.edu>
2014-11-24 02:09:09 -0500

    Add non-Homebrew GPG 1.4.12 install script

---
+++ build_gpg/install_locally.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SHA1HASH=9b78e20328d35525af7b8a9c1cf081396910e937
+GPG_PREFIX=gnupg-1.4.12
+GPG_ARCHIVE=${GPG_PREFIX}.tar.bz2
+export CC="gcc"   # If building on OS X, you will need apple-gcc42
+export CFLAGS="-g -O2"
+
+curl -O http://mirror.switch.ch/ftp/mirror/gnupg/gnupg/${GPG_ARCHIVE}
+shasum -a 1 -c <(echo "${SHA1HASH}  ${GPG_ARCHIVE}")
+
+if [ $? != 0 ]
+then
+  echo "WARNING: HASHES DO NOT MATCH"
+  read -p -n 1 "Continue? [Y/n] " user_input
+
+  if [ user_input = y -o user_input = Y ]
+  then
+    exit 1
+  fi
+fi
+
+tar xf ${GPG_ARCHIVE}
+rm ${GPG_ARCHIVE}
+
+cd ${GPG_PREFIX}
+./configure --disable-dependency-tracking --disable-asm --prefix=${PWD}
+make && make check && make install
