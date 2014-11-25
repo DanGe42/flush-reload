357d6ba1713b953ffec5f0bf58e3777031e0ed8f
Daniel Ge <dange@seas.upenn.edu>
2014-11-24 23:11:46 -0500

    Prettify and update colors in graph.py

--- graph.py
+++ graph.py
@@ -2,10 +2,12 @@ import csv
 import matplotlib.pyplot as plt
 
 CUTOFF = 120
+colors = ['#FF0059', '#2C00E8', '#00F1FF', '#31D40C', '#BF9A00', '#FFFFFF']
 
 with open('out.txt', 'rb') as outfile:
     probereader = csv.reader(outfile, delimiter=' ')
-    rows = [{'slot': int(row[0]), 'addr': int(row[1]), 'time': int(row[2])} for row in probereader]
+    rows = [{'slot': int(row[0]), 'addr': int(row[1]), 'time': int(row[2])}
+            for row in probereader]
     hits = [row for row in rows if row['time'] < CUTOFF]
     seen = {}
     for hit in hits:
@@ -18,6 +20,8 @@ with open('out.txt', 'rb') as outfile:
     print("Slots, tot", len(hits))
     #hits = hits[40:140]
     hits = hits[30:-30]
-    plt.scatter([hit['slot'] for hit in hits], [hit['time'] for hit in hits], c=[hit['addr'] for hit in hits])
+    plt.scatter([hit['slot'] for hit in hits],
+                [hit['time'] for hit in hits],
+                c=[colors[hit['addr']] for hit in hits])
     #plt.plot([hit['slot'] for hit in hits], [hit['time'] for hit in hits])
     plt.show()
