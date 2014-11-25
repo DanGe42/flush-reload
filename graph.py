5fcd38763553d2525a23e64ef9cfc4d8bfe493cf
Nick Meyer <nicholasmeyer@gmail.com>
2014-11-24 23:20:42 -0500

    Fix color scheme

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 
 CUTOFF = 120
-colors = ['#FF0059', '#2C00E8', '#00F1FF', '#31D40C', '#BF9A00', '#FFFFFF']
+colors = ['#FF0059', '#FF0059', '#2C00E8', '#00F1FF', '#00F1FF', '#31D40C', '#BF9A00', '#FFFFFF']
 
 with open('out.txt', 'rb') as outfile:
     probereader = csv.reader(outfile, delimiter=' ')
@@ -18,8 +18,8 @@ with open('out.txt', 'rb') as outfile:
     print("Slots >= 2", len([s for s in seen.keys() if seen[s] >= 2]))
     print("Slots == 0", len([s + 1 for s in seen.keys() if (s + 1) not in seen]))
     print("Slots, tot", len(hits))
-    #hits = hits[40:140]
-    hits = hits[30:-30]
+    hits = hits[40:140]
+    #hits = hits[30:-30]
     plt.scatter([hit['slot'] for hit in hits],
                 [hit['time'] for hit in hits],
                 c=[colors[hit['addr']] for hit in hits])
