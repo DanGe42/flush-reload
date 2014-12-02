d61e5894405e6add9e21ece485068c5b8e1a871a
Nick Meyer <nicholasmeyer@gmail.com>
2014-12-01 21:57:05 -0500

    Improve graph.py

--- graph.py
+++ graph.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 Hit = namedtuple('Hit', ['slot', 'addr', 'time'])
 
 CUTOFF = 120
-square = {'label': 'Square', 'marker': 'x', 'color': '#FF0059', 'addrs': [0, 1, 2]}
+square = {'label': 'Square', 'marker': 'o', 'color': '#FF0059', 'addrs': [0, 1, 2]}
 red = {'label': 'Reduce', 'marker': 'o', 'color': '#2C00E8', 'addrs': [3]}
 mult = {'label': 'Multiply', 'marker': '^', 'color': '#00F1FF', 'addrs': [4, 5]}
 hit_types = [square, red, mult]
@@ -25,8 +25,15 @@ with open('out.txt', 'rb') as outfile:
     print(sum(seen.values()) / len(seen.values()))
     print("Slots >= 2 {:d}".format(len([s for s in seen.keys() if seen[s] >= 2])))
     print("Slots == 0 {:d}".format(len([s + 1 for s in seen.keys() if (s + 1) not in seen])))
-    print("Slots, tot {:d}".format(len(hits)))
+    print("Slots, tot {:d}".format(len(seen)))
     hits = hits[30:-30]
+
+    for hit_type in hit_types:
+        hits_of_type = [hit for hit in hits if hit.addr in hit_type['addrs']]
+        slot_to_hits = {}
+        for hit in hits_of_type:
+            slot_to_hits[hit.slot] = hit
+        print("{:s} {:d}".format(hit_type['label'], len(slot_to_hits.keys())))
 
     fig = plt.figure()
     axis = fig.add_subplot(111)
