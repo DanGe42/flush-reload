9aa4c37964bfbea0d9912d0b85b3e9a24ca7567e
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2016-06-08 12:20:15 +0200

    Automatize the key generation

--- build_gpg/install_locally.sh
+++ build_gpg/install_locally.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 SHA1HASH=9b78e20328d35525af7b8a9c1cf081396910e937
+SHA256HASH=4f03ca6902aaee79d0eda00eea0fefde7db9eb005d8ffd54fac9806bc24050ec
 GPG_PREFIX=gnupg-1.4.12
 GPG_ARCHIVE=${GPG_PREFIX}.tar.bz2
+HASH=sha256sum
 export CC=gcc   # If building on OS X, you will need apple-gcc42
 export CFLAGS="-g -O2 -std=c89"
 
+# Download GPG 1.4.12
 curl -O http://mirror.switch.ch/ftp/mirror/gnupg/gnupg/${GPG_ARCHIVE}
-shasum -a 1 -c <(echo "${SHA1HASH}  ${GPG_ARCHIVE}")
+shasum -a 256 -c <(echo "${SHA256HASH}  ${GPG_ARCHIVE}")
 
 if [ $? != 0 ]
 then
@@ -22,6 +25,12 @@ fi
 tar xf ${GPG_ARCHIVE}
 rm ${GPG_ARCHIVE}
 
+# Install GPG locally
 cd ${GPG_PREFIX}
 ./configure --disable-dependency-tracking --disable-asm --prefix=${PWD}
-make && make check # && make install
+make && make check
+
+# Generate the secret key
+cd ..
+./gnupg-1.4.12/g10/gpg --gen-key --batch keyparams
+./gnupg-1.4.12/g10/gpg --import flushreload.sec
