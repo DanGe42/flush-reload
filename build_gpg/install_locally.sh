23738b5d6225040be5aae392d084057f302b6148
Daniel Ge <daniel@dge.io>
2016-06-11 14:44:21 -0700

    Clean up shell scripts and fix Shellcheck warnings

--- build_gpg/install_locally.sh
+++ build_gpg/install_locally.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Use this script if you are using Linux or if OS X Homebrew is not installed.
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
 SHA1HASH=9b78e20328d35525af7b8a9c1cf081396910e937
 GPG_PREFIX=gnupg-1.4.12
 GPG_ARCHIVE=${GPG_PREFIX}.tar.bz2
@@ -11,9 +17,9 @@ shasum -a 1 -c <(echo "${SHA1HASH}  ${GPG_ARCHIVE}")
 if [ $? != 0 ]
 then
   echo "WARNING: HASHES DO NOT MATCH"
-  read -p -n 1 "Continue? [Y/n] " user_input
+  read -p -r -n 1 "Continue? [Y/n] " user_input
 
-  if [ user_input = y -o user_input = Y ]
+  if [ "$user_input" = y ] ||  [ "$user_input" = Y ]
   then
     exit 1
   fi
@@ -23,5 +29,5 @@ tar xf ${GPG_ARCHIVE}
 rm ${GPG_ARCHIVE}
 
 cd ${GPG_PREFIX}
-./configure --disable-dependency-tracking --disable-asm --prefix=${PWD}
+./configure --disable-dependency-tracking --disable-asm --prefix="${PWD}"
 make && make check && make install
--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+set -o errexit
+set -o xtrace
+set -o nounset
+
 GPG=$HOME/gnupg-1.4.12/bin/gpg
 ADDR=docs/addr/osx.txt
 MESSAGE=message.txt
 OUT=out.txt
 CYCLES=$1
 
-bin/probe ${GPG} ${ADDR} ${OUT} ${CYCLES} &
+bin/probe "${GPG}" ${ADDR} ${OUT} "${CYCLES}" &
 PROBE_PID=$!
 
 sleep 0.01
@@ -14,7 +18,7 @@ sleep 0.01
 GPG_PID=$!
 
 trap "echo 'Received signal'; kill -TERM ${PROBE_PID} ${GPG_PID}" \
-    SIGINT SIGQUIT
+    INT QUIT
 
 wait ${PROBE_PID}
 wait ${GPG_PID}
@@ -22,5 +26,5 @@ wait ${GPG_PID}
 python graph.py &
 GRAPH_PID=$!
 
-trap "echo 'Killing python'; kill -TERM ${GRAPH_PID}" SIGINT SIGQUIT
+trap "echo 'Killing python'; kill -TERM ${GRAPH_PID}" INT QUIT
 wait ${GRAPH_PID}
